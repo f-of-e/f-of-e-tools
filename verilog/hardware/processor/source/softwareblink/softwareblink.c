185305bd598e9974f25e9d36044dc97970a2fac6
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2019-05-17 05:33:12 +0100

    Updated softwareblink.c to include more comments and simplify (simply toggle all the led pins, simpler than what was there before)

--- verilog/hardware/processor/source/softwareblink/softwareblink.c
+++ verilog/hardware/processor/source/softwareblink/softwareblink.c
@@ -1,25 +1,33 @@
 int
 main(void)
 {
+	/*
+	 *	Reading from the special address 0x2000 will cause sail to
+	 *	set the value of 8 of the FPGA's pins to the byte written
+	 *	to the address. See the PCF file for how those 8 pins are
+	 *	mapped.
+	 */
 	volatile unsigned int *		debugLEDs = (unsigned int *)0x2000;
-	int				i = 0;
 
 	enum
 	{
-		kSpinDelay = 4000,
+		kSpinDelay = 400000,
 	};
 
 	while(1)
 	{
+		*debugLEDs = 0xFF;
+
 		/*
 		 *	Spin
 		 */
 		for (int j = 0; j < kSpinDelay; j++);
 
+		*debugLEDs = 0x00;
+
 		/*
-		 *	Set the debug LEDs with the lower 8 bits of the
-		 *	free-running counter.
+		 *	Spin
 		 */
-		*debugLEDs = i++ & 0xFF;
+		for (int j = 0; j < kSpinDelay; j++);		
 	}
 }
