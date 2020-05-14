a6975040c7c53b90ffd273785a7d100f590f06e4
Richard Fleck <rfleck98@gmail.com>
2020-05-14 11:42:33 +0000

    'no-op' implemented in idle loop

--- verilog/hardware/processor/source/softwareidle/softwareidle.c
+++ verilog/hardware/processor/source/softwareidle/softwareidle.c
@@ -2,13 +2,11 @@ int
 main(void)
 {
 	/*
-	 * do nothing for 1000000 clock cycles
+	 * execute a no-op in a loop
 	 */
 
 	while(1)
 	{
-	
-	//	for (int j = 0; j < 1000000; j++);
-			
+	asm("nop");
 	}
 }
