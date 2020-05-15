727a5e57f43d83b11378dd9553e1f8fe2859f7d7
Richard Fleck <rfleck98@gmail.com>
2020-05-15 11:11:57 +0000

    renamed idle and added calculate_e

---
+++ verilog/hardware/processor/source/calculate_e/calculate_e.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+ 
+#define EPSILON 1.0e-15
+//calculates the value of e to precision 1.0e^-15 and loops
+//by approximation e = 1 + 1/2! + 1/3! + ...
+
+int main() {
+    while(1){
+   	unsigned long long fact = 1; //stores a 'running' value of 1/n!
+   	double e = 2.0, e0;    //e stores current value, e0 stores previous value
+    	int n = 2;    
+    	while(fabs(e-e0) >= EPSILON){
+		//while absolute improvement in precision is greater than 1.0e^-15
+        	e0 = e; 	    //store previous value of e in e0
+        	fact *= n++;	    //update n!
+        	e += 1.0 / fact;    //add next value of 1/n! to e
+    	}
+
+    return 0;
+    }
+}
