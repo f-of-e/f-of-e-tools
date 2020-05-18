e7ae91d901d254b8ad789bc03a8b819c4b95bae8
Richard Fleck <rfleck98@gmail.com>
2020-05-18 14:57:52 +0000

    added folder for performance tests

---
+++ verilog/hardware/processor/source/rf_performance/rf_calculate_e/rf_calculate_e.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+//calculates the value of e to precision 1.0e^-15
+//by approximation e = 1 + 1/2! + 1/3! + ...
+
+int main(void) {
+   	unsigned long long fact = 1; //stores a 'running' value of 1/n!
+   	double e = 2.0, e0;    //e stores current value, e0 stores previous value
+    	int n = 2;    
+    	do {
+		//while absolute improvement in precision is greater than 1.0e^-15
+        	e0 = e; 	    //store previous value of e in e0
+        	fact *= n++;	    //update n!
+        	e += 1.0 / fact;    //add next value of 1/n! to e
+    	}
+	while(fabs(e-e0) >= 0.000000000000001);
+    
+    return 0;
+}
+
---
+++ verilog/hardware/processor/source/rf_performance/rf_calculate_e/run.m
@@ -0,0 +1,5 @@
+newnode		riscv
+sizemem		65536	
+srecl		"rf_calculate_e-sf.sr"
+run
+on
