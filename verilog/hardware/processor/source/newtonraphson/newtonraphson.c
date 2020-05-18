3dd17d10fb80349751e1d1ac4369560c7b468193
Richard Fleck <rfleck98@gmail.com>
2020-05-18 14:01:18 +0000

    added newton raphson test

---
+++ verilog/hardware/processor/source/newtonraphson/newtonraphson.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+double F(double x)
+{//our function
+    return ((x)*(x)-5);
+}
+double Fd(double x)
+{//function's differentiation
+    return (2*(x));
+}
+int main(void)
+{
+   while(1){
+    double h,root,x1;
+    double x0 = 2;
+    double err = 0.0000000001;
+    int miter = 7;
+    int iter = 1;
+   
+    while(iter<=miter)
+    {
+        h=F(x0)/Fd(x0);//calculatinf f(x)/f'(x)as we do in Newton Raphson method
+        x1=x0-h;//x1=x0-f(x)/f'(x)
+        if(fabs(h)<err)//If the difference between the 2 approximations is below the max error
+        {
+            root=x1;//then make the approximation as the root
+            break;
+        }
+        else
+        x0=x1;
+        iter++;
+    }
+   }
+    return 0;
+    
+}
