4ce1df37a1c8c3620b5df681643c861863350954
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2020-05-27 19:16:09 +0100

    Move definitions into header file

---
+++ verilog/hardware/processor/source/softwareblink/softwareblink.h
@@ -0,0 +1,12 @@
+enum
+{
+	kSpinDelay = 400000,
+};
+
+/*
+ *	Reading from the special address 0x2000 will cause sail to
+ *	set the value of 8 of the FPGA's pins to the byte written
+ *	to the address. See the PCF file for how those 8 pins are
+ *	mapped.
+ */
+volatile unsigned int *		gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
