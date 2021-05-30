334b5daaa6c9810fb35a9456eb43f313bda646b4
Jakub Suchanek <suchanek989@seznam.cz>
2021-05-30 23:19:57 +0000

    maybe fix pll

--- verilog/hardware/processor/improved_verilog/config.v
+++ verilog/hardware/processor/improved_verilog/config.v
@@ -1,26 +1,26 @@
 // DSP blocks
 
-// `define USE_ADDER_DSP
-// `define USE_SUBTRACTOR_DSP
+//`define USE_ADDER_DSP
+//`define USE_SUBTRACTOR_DSP
 
  
 // Clock
 
 //`define USE_PLL_CLK
 `define CLK_PLL_DIVF 0
-`define CLK_PLL_DIVR 1
+`define CLK_PLL_DIVR 2
 `define CLK_PLL_DIVQ 0
-`define CLK_PLL_DIV_REG 2  // must be undefined or at least 1
+`define CLK_PLL_DIV_REG 1  // must be undefined or at least 1
 
 `define CLK_NOPLL_DIV "0b11"
 
 
 // branch prediction
 
-//`define USE_CORRELATING
+`define USE_CORRELATING
 
 
 // memory
 
-`define USE_SMALL_DATA_ADDR
-`define USE_MEMORY_OPTIMIZATIONS
+//`define USE_SMALL_DATA_ADDR
+//`define USE_MEMORY_OPTIMIZATIONS
--- verilog/hardware/processor/improved_verilog/pll_clk.v
+++ verilog/hardware/processor/improved_verilog/pll_clk.v
@@ -1,48 +1,3 @@
-/*
-	Authored 2018-2019, Ryan Voo.
-
-	All rights reserved.
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions
-	are met:
-
-	*	Redistributions of source code must retain the above
-		copyright notice, this list of conditions and the following
-		disclaimer.
-
-	*	Redistributions in binary form must reproduce the above
-		copyright notice, this list of conditions and the following
-		disclaimer in the documentation and/or other materials
-		provided with the distribution.
-
-	*	Neither the name of the author nor the names of its
-		contributors may be used to endorse or promote products
-		derived from this software without specific prior written
-		permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
-*/
-
-
-
-/*
- *	Description:
- *
- *		This module implements an adder for use by the branch unit
- *		and program counter increment among other things.
- */
-
 
 module pll_clk(clk_hf, clk);
 	input clk_hf;
@@ -53,12 +8,8 @@ module pll_clk(clk_hf, clk);
 	`else
 		wire[0:0] clk_mf;
 	`endif
-
-	// wire RST_i = 1'b0
 	
-	// assign		out = input1 + input2;
-
-	/* Instantiating DSP module*/
+	/*
 	SB_PLL40_CORE pll40_i(
 		.REFERENCECLK(clk_hf),
 		.BYPASS(1'b0),
@@ -88,11 +39,29 @@ module pll_clk(clk_hf, clk);
 	defparam pll40_i.ENABLE_ICEGATE = 1'b0;
 	defparam pll40_i.TEST_MODE = 1'b0;
 	defparam pll40_i.EXTERNAL_DIVIDE_FACTOR = 1;
+	*/
+	
+	wire locked;
+	assign locked = 1'b0;
+	
+	SB_PLL40_CORE #(
+		.FEEDBACK_PATH("SIMPLE"),
+		.DIVR(4'b0010),		// DIVR =  2
+		.DIVF(7'b0111111),	// DIVF = 63
+		.DIVQ(3'b110),		// DIVQ =  6
+		.FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
+	) pll40_i (
+		.LOCK(locked),
+		.RESETB(1'b1),
+		.BYPASS(1'b0),
+		.REFERENCECLK(clk_hf),
+		.PLLOUTCORE(clk_mf[0])
+	);
 	
 	`ifdef CLK_PLL_DIV_REG
 		reg[`CLK_PLL_DIV_REG-1:0] divider_regs;
 		
-		integer i;
+		genvar i;
 		for (i = 0; i < `CLK_PLL_DIV_REG - 1; i = i + 1) begin
 			assign clk_mf[i+1] = divider_regs[i];
 			
