833d30bbca4a80dd81dc80ce3d919bdd415489bb
Marcos <marcostapia4@gmail.com>
2021-06-02 10:13:40 +0000

    Comment out HARD IP components since they cannot be simulated

--- verilog/hardware/processor/sail-simulation/improved_verilog/config.v
+++ verilog/hardware/processor/sail-simulation/improved_verilog/config.v
@@ -1,12 +1,12 @@
 // DSP blocks
 
-`define USE_ADDER_DSP
-`define USE_SUBTRACTOR_DSP
+//`define USE_ADDER_DSP
+//`define USE_SUBTRACTOR_DSP
 
  
 // Clock
 
-`define USE_PLL_CLK
+//`define USE_PLL_CLK
 `define CLK_PLL_DIVF 0
 `define CLK_PLL_DIVR 1
 `define CLK_PLL_DIVQ 0
@@ -17,7 +17,7 @@
 
 // branch prediction
 
-//`define USE_CORRELATING
+`define USE_CORRELATING
 //`define USE_ONE_BIT //Cannot be defined if USE_CORRELATING is defined too!
 //`define USE_BRANCH 
 //`define USE_STATIC //Cannot be defined if USE_CORRELATING is defined too!
