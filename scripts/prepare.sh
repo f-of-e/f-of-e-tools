b748dc2644b3a275c7f9d0d8e1bb41ea99568bd1
Jason <jasonrbrown@virginmedia.com>
2022-05-19 13:35:35 +0000

    softwareblink compile

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yosys -p "synth_ice40 -blif $1.blif; write_json $1.json" $1.v > $1_yosys_log.txt
-nextpnr-ice40 --up5k --package uwg30 --json $1.json --pcf $1.pcf --asc $1.asc > $1_nextpnr_log.txt
-icepack $1.asc $1.bin > $1_icepack_log.txt
+yosys -p "synth_ice40 -blif $1.blif; write_json $1.json" $1.v > $1_yosys_log.txt 2>&1
+nextpnr-ice40 --up5k --package uwg30 --json $1.json --pcf $1.pcf --asc $1.asc > $1_nextpnr_log.txt 2>&1
+icepack $1.asc $1.bin > $1_icepack_log.txt 2>&1
 
