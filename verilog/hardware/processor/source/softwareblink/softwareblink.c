0f5172e31e967adf12d50c8cc14ef52117ec63a4
Jason <jasonrbrown@virginmedia.com>
2022-05-20 08:30:24 +0000

    Moved comment

--- verilog/hardware/processor/source/softwareblink/softwareblink.c
+++ verilog/hardware/processor/source/softwareblink/softwareblink.c
@@ -3,6 +3,15 @@
 int
 main(void)
 {
+    /*
+     *	Reading from the special address 0x2000 will cause sail to
+     *	set the value of 8 of the FPGA's pins to the byte written
+     *	to the address. See the PCF file for how those 8 pins are
+     *	mapped.
+     */
+
+    volatile unsigned int *		gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
+
 	/*
 	 *	Reading from the special address pointed to by
 	 *	kDebugLedsMemoryMappedRegister will cause the processor to
@@ -10,8 +19,6 @@ main(void)
 	 *	to the address. See the PCF file for how those 8 pins are
 	 *	mapped.
 	 */
-
-    volatile unsigned int *		gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
 
 	while(1)
 	{
--- verilog/hardware/processor/source/softwareblink/softwareblink.h
+++ verilog/hardware/processor/source/softwareblink/softwareblink.h
@@ -2,10 +2,3 @@ enum
 {
 	kSpinDelay = 400000, // 400000
 };
-
-/*
- *	Reading from the special address 0x2000 will cause sail to
- *	set the value of 8 of the FPGA's pins to the byte written
- *	to the address. See the PCF file for how those 8 pins are
- *	mapped.
- */
